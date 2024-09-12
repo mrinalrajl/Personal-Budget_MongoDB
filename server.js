0c3a559481cf938acaf2b557772a3c29543a35e6
Mrinal <mrinalrajnaidu@gmail.com>
2024-09-12 02:09:32 -0400

    Assignemnt_4

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { title } = require('process');
+const app = express();
+const port = 3000;
+
+app.use('/', express.static('public'));
+
+
+
+const budget = {
+    myBudget: [
+    {
+        title: 'Eat Out',
+        budget: 30
+    },
+    {
+        title: 'Rent',
+        budget: 440
+    },
+    {
+        title: 'Groceries',
+        budget: 90
+    },
+    {
+        title: 'Fuel',
+        budget: 40
+    }
+
+]
+};
+app.get('/hello', (req, res) => {
+    res.send('Hello World! From Mrinal');
+});
+
+app.get('/budget', (req, res) => {
+    res.json(budget);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
